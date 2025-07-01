1a3b4f763c3553fb1daecf6aa0bcbe1de83f4cca
soo <soojeong.kim121@gmail.com>
2025-07-01 19:44:51 +0100

    move arrow

--- app/my10/page.tsx
+++ app/my10/page.tsx
@@ -26,7 +26,7 @@ export default function My10Page() {
       {/* Content */}
       <div className="flex flex-col items-center justify-center min-h-screen pt-16 px-16 pb-32 md:pb-16 max-w-4xl mx-auto">
         {/* Title */}
-        <h1 className="text-6xl font-bold mb-8 text-center" style={{ color: 'rgb(215, 92, 20)' }}>my 10</h1>
+        <h1 className="text-9xl font-bold mb-8 text-center" style={{ color: 'rgb(215, 92, 20)' }}>my 10</h1>
         
         {/* Downward arrow */}
         <div className="text-4xl mb-8 text-center" style={{ color: 'rgb(215, 92, 20)' }}>↓</div>
@@ -40,6 +40,11 @@ export default function My10Page() {
         <div className="w-full">
           {/* Top 10 items will be added here */}
         </div>
+      </div>
+
+      {/* Bottom arrow */}
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
+        <div className="text-6xl font-black text-black">V</div>
       </div>
     </div>
   )
